6afddce4234537088d5a429ef1ebbb0978a4515f
Denise Bischof <denisebischof@web.de>
2018-07-25 01:53:54 +0100

    forgot to reupload, sorry
    minor bugfixes

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dialogueUI;
+    public Text dialogueText;
+
+    public bool dialogueActive;
+
+    public string[] dialogueLines;
+
+    public int currentLine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            // dialogueUI.SetActive(false);
+            // dialogueActive = false;
+
+            currentLine++;
+        }
+
+        if(currentLine >= dialogueLines.Length)
+        {
+            dialogueUI.SetActive(false);
+            dialogueActive = false;
+
+            currentLine = 0;
+
+        }
+
+        dialogueText.text = dialogueLines[currentLine];
+	}
+
+    public void showBox(string dialogue)
+    {
+        dialogueActive = true;
+        dialogueUI.SetActive(true);
+        dialogueText.text = dialogue;
+    }
+
+    public void showDialogue()
+    {
+        dialogueActive = true;
+        dialogueUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/dialogueHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueHolder : MonoBehaviour {
+
+    private DialogueManager dialogueManager;
+
+    //public string dialogue;
+
+    public string[] dialogueLines;
+
+
+	// Use this for initialization
+	void Start () {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+      if(other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                // dialogueManager.showBox(dialogue);
+                if (!dialogueManager.dialogueActive)
+                {
+                    dialogueManager.dialogueLines = dialogueLines;
+                    dialogueManager.currentLine = 0;
+                    dialogueManager.showDialogue();
+                }
+            }
+        } 
+    }
+}
